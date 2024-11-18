c65f73736a7cd1848e7f851af505e5252a5bbb7e
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2024-11-18 13:21:14 +0100

    Implement case insensitve search

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,49 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("{content}");
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}")
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut lines = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            lines.push(line);
+        }
+    }
+
+    lines
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut lines = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            lines.push(line);
+        }
+    }
+
+    lines
 }
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -22,6 +55,43 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let case_sensitive = env::var("CASE_SENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            file_path,
+            case_sensitive,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+
+        let content = "\
+Rust:
+safe, fast and productive.
+pick three.
+    ";
+
+        assert_eq!(vec!["safe, fast and productive."], search(query, content))
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "RusT";
+
+        let content = "\
+    Rust:
+    safe, fast and productive.
+    pick three.
+        ";
+
+        assert_eq!(vec!["Rust:"], search_case_insensitive(query, content))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {}", err);
+        eprintln!("Problem parsing args: {}", err);
         process::exit(1)
     });
 
     println!("searching for {} in {}", config.query, config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1)
     }
 }
